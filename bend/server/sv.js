507938d50d4d7731eaaf59bd2c70da35fa460a02
ealoticopve <ricardodavid120@gmail.com>
2022-05-24 23:09:00 +0100

    dotenv applied

--- bend/server/sv.js
+++ bend/server/sv.js
@@ -1,8 +1,13 @@
+import 'dotenv/config'
 import express from 'express'
-import env from 'dotenv/config'
+
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send(`Hello World! ${PORT}`)
