5fac2b6ea024df13f91da35b122471e28ff74a0d
ealoticopve <ricardodavid120@gmail.com>
2022-06-01 03:55:21 +0100

    Ending API setup

--- bend/server/sv.js
+++ bend/server/sv.js
@@ -1,17 +1,21 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
+import userRoutes from '../routes/auth/ManUsers.js'
+import { connectDatabase } from '../db/conn.js'
+
+connectDatabase()
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors())
 
 
-app.get('/', (req, res) => {
-    res.send(`Hello World! ${PORT}`)
-})
+app.use('/authapi', userRoutes)           //prefix /api/users
 
 app.listen(PORT, () => {
-    console.log(`Testing dev ${PORT}`)
+    console.log(`Server listening in http://localhost:${PORT}`)
 })
